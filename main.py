b610c3b9290d475e36dffe4ace189b25a98e010b
HemanthKumarPavuluri <phk.phk007@gmail.com>
2025-01-11 00:56:37 -0600

    word counter function added

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ def main():
     try:
         with open("books/frankenstein.txt") as f:
             file_contents = f.read()
-            print(file_contents)
+            count_words(file_contents)
     except FileNotFoundError:
         print("The file 'frankenstein.txt' was not found. Please check the path.")
 
+def count_words(text):
+    num_of_words = len(text.split())
+    print(num_of_words)
 if __name__ == "__main__":
     main()
